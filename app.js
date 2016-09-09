fc931f7fc323f600ecc5fdae41b1b70d032a540a
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-09-09 12:03:17 -0400

    webpack timing plugin

--- app.js
+++ app.js
@@ -24,6 +24,47 @@ module.exports = {
   babel: { presets: [jsStandards] },
   recordsPath: path.join(__dirname, 'records.json'),
   plugins: [
+    {
+      apply: function(compiler) {
+        var start, start2;
+        compiler.plugin(['watch-run', 'run'], function(compiler, cb) {
+          start = Date.now();
+          start2 = Date.now();
+          cb();
+        });
+        compiler.plugin('make', function(compilation, cb) {
+          console.log('pre-make', Date.now() - start);
+          start = Date.now();
+          compilation.plugin('seal', function() {
+            console.log('make', Date.now() - start);
+            start = Date.now();
+          });
+          compilation.plugin('optimize-tree', function(chunks, modules, cb) {
+            console.log('pre optimize-tree', Date.now() - start);
+            start = Date.now();
+            cb();
+          });
+          compilation.plugin('after-optimize-tree', function() {
+            console.log('optimize-tree', Date.now() - start);
+            start = Date.now();
+          });
+          compilation.plugin('after-optimize-assets', function() {
+            console.log('post optimize-tree', Date.now() - start);
+            start = Date.now();
+          });
+          cb();
+        });
+        compiler.plugin('emit', function(compilation, cb) {
+          console.log('after-compile', Date.now() - start);
+          start = Date.now();
+          cb();
+        })
+        compiler.plugin('done', function() {
+          console.log('emit', Date.now() - start);
+          console.log(Date.now() - start2);
+        });
+      },
+    },
     new HardSource({ cacheDirectory: '../cache' })
   ]
 }
